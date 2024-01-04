4e2b6425b12385c249f3f286020d8ae7c8bcbb2e
saurabhpawar711 <saurabhpawar71100@gmail.com>
2024-01-04 21:25:20 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function ExpenseItem() {
+  return (
+    <div>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movie Rs 200</p>
+    </div>
+  )
+}
